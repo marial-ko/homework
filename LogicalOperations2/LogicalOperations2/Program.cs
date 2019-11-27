79962e3e48879c0c8723e866b9d69ad885553208
MariaL <lysenko-maria75@mail.ru>
2019-11-27 20:49:41 +0500

    adding hometasks

--- LogicalOperations2/LogicalOperations2/Program.cs
+++ LogicalOperations2/LogicalOperations2/Program.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace helloworld
+namespace LogicalOperations2
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
         }
     }
 }
