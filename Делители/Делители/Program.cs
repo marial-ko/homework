d2bdc6cc8006022f66dc1790d00b5b7990b4c7e3
MariaL <lysenko-maria75@mail.ru>
2019-12-22 12:34:32 +0500

    исправленные задачи 4 и 6 по операторам цикла

--- Делители/Делители/Program.cs
+++ Делители/Делители/Program.cs
@@ -10,25 +10,29 @@ namespace Делители
     {
         static void Main(string[] args)
         {
-            var sum = 0;
+           
             Console.WriteLine("Введите a");
             var a = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите b");
             var b = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите k (2<=k<=10)");
             var k = int.Parse(Console.ReadLine());
+            
 
             for (var i = a; i <= b; i++)
+            {
+                var sum = 0;
                 for (var j = 1; j <= i; j++)
                 {
+
                     if (i % j == 0)
                         sum = sum + j;
 
-                    if (sum % k == 0)
-                        Console.WriteLine("Число, сумма делителей которого кратна {1}: {0}", i, k);
                 }
-                
-                Console.ReadKey();
+                if (sum % k == 0)
+                    Console.WriteLine("Число, сумма делителей которого кратна {1}: {0}", i, k);
+            }
+            Console.ReadKey();
         }
     }
 }
